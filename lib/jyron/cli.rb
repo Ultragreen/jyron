06c641878c501b7f99742e5b88675de3035dc2a5
Romain Georges <romain@ultragreen.net>
2021-05-16 22:48:40 +0200

    refacto

--- lib/jyron/cli.rb
+++ lib/jyron/cli.rb
@@ -1,6 +1,7 @@
 module JYRon
 class CLI < Thor
   include JYRon::Inputs
+  include JYRon
   desc "mediator", "say hello to NAME"
   long_desc <<-LONGDESC
     Mediator
@@ -10,9 +11,20 @@ class CLI < Thor
     option input, :type => :string, :banner => params.banner,  :desc = > params.desc
   end
   def mediator
-    data = File::readlines("./spec/test.json").join("\n")
+    if options[:file] then
+      data = readfile(options[:file])
+    else
+      puts "A data file is required ( --file|-f <filename> )"
+      exit 2
+    end
     test = JYRon::Mediator::new adapters: [:stringify_keys, :camelcase_keys]
-    puts test.from_json(data).jsonpath!("$..Maths").to_json
+    inputs = options.select{|value| value.to_s =~ /^from_/} & CLI_INPUTS.keys
+    unless inputs.size < 2 then
+      puts "Only one inputs type requiered"
+      exit 2
+    end
+    input = inputs.first
+    puts test.send(input, data).jsonpath!("$..Maths").to_json
   end
 end
 
