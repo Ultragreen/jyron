5c96f0b3310ff4d211087a712b9fd0f6bdb5dc24
Romain Georges <romain@ultragreen.net>
2021-05-16 14:42:20 +0200

    bin cmd

--- lib/jyron.rb
+++ lib/jyron.rb
@@ -1,10 +1,6 @@
 
-require 'requierements'
+require 'dependencies'
 
 module JYRon
   include JYRon::Mediator
 end
-
-data = File::readlines("./spec/test.json").join("\n")
-test = JYRon::Mediator::new adapters: [:stringify_keys, :camelcase_keys]
-puts test.from_json(data).jsonpath!("$..Maths").to_json
